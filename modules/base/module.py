f18939be400ab25a75c35d380dadb3fd41877336
Sal0hc1n <sollsharp@gmail.com>
2017-06-13 11:34:28 +0200

    module.py: implemented json functionalities (such as 'get_path'), 'get_conditions' and 'get_actions'

--- modules/base/module.py
+++ modules/base/module.py
@@ -2,8 +2,8 @@
 Module.py
 @author: Nicholas Sollazzo
 @mail: sollsharp@gmail.com
-@version: 1.1
-@date: 11/06/17
+@version: 1.2
+@date: 13/06/17
 @status: WRK
 '''
 
@@ -11,9 +11,25 @@ Module.py
 class Module(object):
     """docstring for Module."""
 
-    def __init__(self, args):
+    def __init__(self):
+        self.path = ''
+        self.jsn = pyJson(self.path)
+        self.jsn.set({'module_name': 'module'})
         self.cond = False  # False: the module doesn't have conditions
         self.act = False  # False: the module doesn't have actions
+        self.COND = []
+        self.ACT = []
+
+    def get_path(self):
+        return self.path
+
+    def get_conditions(self):
+        if cond:
+            return COND
+
+    def get_actions(self, arg):
+        if act:
+            return COND
 
     def condition(self, arg):  # select the condition that has to be executed
         switcher = {
