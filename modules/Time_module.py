cf3dcd48fef5c581dd42d5e3297f69900190e627
Sal0hc1n <sollsharp@gmail.com>
2017-06-13 11:36:15 +0200

    Time_module.py: implemented json functionalities from 'module.py'

--- modules/Time_module.py
+++ modules/Time_module.py
@@ -2,34 +2,50 @@
 Time_module.py
 @author: Nicholas Sollazzo
 @mail: sollsharp@gmail.com
-@version: 1.0
-@date: 11/06/17
+@version: 0.2
+@date: 13/06/17
 @status: TBT
 '''
 
 import time
 
-from module import Module
+from modules.base.module import Module
+from utils.pyJson import pyJson
 
 
 class Time(Module):
     """docstring for Time."""
 
-    def __init__(self, args):
+    def __init__(self):
+        self.path = 'data/modules_data/time.json'
+        self.jsn = pyJson(self.path)
+        self.jsn.set({'module_name': 'Time_module'})
         self.cond = True
         self.act = True
+        self.COND = ['current_time', 'current_year', 'current_month', 'current_month_day',
+                     'current_hour', 'current_min', 'current_sec', 'current_week_day', 'current_year_day']
+        self.ACT = ['current_time', 'current_time_formatted', 'delay']
 
     def condition(self, arg):
         switcher = {
-            'current_time': current_time
+            self.COND[0]: current_time,
+            self.COND[1]: current_year,
+            self.COND[2]: current_month,
+            self.COND[3]: current_month_day,
+            self.COND[4]: current_hour,
+            self.COND[5]: current_min,
+            self.COND[6]: current_sec,
+            self.COND[7]: current_week_day,
+            self.COND[8]: current_year_day
         }
         funAction = switcher[arg]
         funAction()
 
     def action(self, arg):
         switcher = {
-            'current_time': current_time,
-            'current_time_formatted': current_time_formatted
+            self.ACT[0]: current_time,
+            self.ACT[1]: current_time_formatted,
+            self.ACT[2]: delay
         }
         funAction = switcher[arg]
         funAction()
@@ -65,3 +81,7 @@ class Time(Module):
     def current_time_formatted(self):
         '''Sun Jun 11 17:58:32 2017'''
         return time.asctime(time.localtime(time.time()))
+
+    def delay(self):
+        seconds = self.jsn.get('seconds')
+        time.delay(seconds)
